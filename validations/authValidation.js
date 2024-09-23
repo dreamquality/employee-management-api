fb2aa61290a9751c7662be9fcf88194da9a973a2
Alex <130073078+dreamquality@users.noreply.github.com>
2024-09-23 20:40:45 +0300

    Add files via upload

--- validations/authValidation.js
+++ validations/authValidation.js
@@ -2,14 +2,35 @@
 const { body } = require('express-validator');
 
 exports.registerValidation = [
-  body('email').isEmail().withMessage('Некорректный email'),
-  body('password').isLength({ min: 6 }).withMessage('Пароль должен быть не менее 6 символов'),
-  body('firstName').notEmpty().withMessage('Имя обязательно'),
-  body('lastName').notEmpty().withMessage('Фамилия обязательна'),
-  body('middleName').notEmpty().withMessage('Отчество обязательно'),
-  body('birthDate').isDate().withMessage('Некорректная дата рождения'),
-  body('phone').notEmpty().withMessage('Телефон обязателен'),
-  body('programmingLanguage').notEmpty().withMessage('Язык программирования обязателен'),
+  body('email')
+    .isEmail().withMessage('Некорректный email')
+    .isLength({ min: 5, max: 80 }).withMessage('Email должен быть от 5 до 80 символов'),
+
+  body('password')
+    .isLength({ min: 6, max: 20 }).withMessage('Пароль должен быть от 6 до 20 символов'),
+
+  body('firstName')
+    .isLength({ min: 2, max: 40 }).withMessage('Имя должно быть от 2 до 40 символов')
+    .notEmpty().withMessage('Имя обязательно'),
+
+  body('lastName')
+    .isLength({ min: 2, max: 40 }).withMessage('Фамилия должна быть от 2 до 40 символов')
+    .notEmpty().withMessage('Фамилия обязательна'),
+
+  body('middleName')
+    .isLength({ min: 2, max: 40 }).withMessage('Отчество должно быть от 2 до 40 символов')
+    .notEmpty().withMessage('Отчество обязательно'),
+
+  body('birthDate')
+    .isDate().withMessage('Некорректная дата рождения'),
+
+  body('phone')
+    .isLength({ max: 50 }).withMessage('Телефон не должен быть длиннее 50 символов')
+    .notEmpty().withMessage('Телефон обязателен'),
+
+  body('programmingLanguage')
+    .isLength({ max: 100 }).withMessage('Язык программирования не должен быть длиннее 100 символов')
+    .notEmpty().withMessage('Язык программирования обязателен'),
 ];
 
 exports.loginValidation = [
--- validations/userValidation.js
+++ validations/userValidation.js
@@ -2,37 +2,92 @@
 const {  body, query, validationResult } = require('express-validator');
 
 exports.userUpdateValidation = [
-  body('firstName').optional().notEmpty().withMessage('Имя не может быть пустым'),
-  body('lastName').optional().notEmpty().withMessage('Фамилия не может быть пустой'),
-  body('middleName').optional().notEmpty().withMessage('Отчество не может быть пустым'),
-  body('birthDate').optional().isDate().withMessage('Некорректная дата рождения'),
-  body('phone').optional().notEmpty().withMessage('Телефон не может быть пустым'),
-  body('email').optional().isEmail().withMessage('Некорректный email'),
-  body('programmingLanguage').optional().notEmpty().withMessage('Язык программирования не может быть пустым'),
-  body('country').optional().notEmpty().withMessage('Страна не может быть пустой'),
-  body('bankCard').optional().notEmpty().withMessage('Банковская карта не может быть пустой'),
+  body('firstName')
+    .optional()
+    .isLength({ min: 2, max: 40 }).withMessage('Имя должно быть от 2 до 40 символов')
+    .notEmpty().withMessage('Имя не может быть пустым'),
+  
+  body('lastName')
+    .optional()
+    .isLength({ min: 2, max: 40 }).withMessage('Фамилия должна быть от 2 до 40 символов')
+    .notEmpty().withMessage('Фамилия не может быть пустой'),
+
+  body('middleName')
+    .optional()
+    .isLength({ min: 2, max: 40 }).withMessage('Отчество должно быть от 2 до 40 символов')
+    .notEmpty().withMessage('Отчество не может быть пустым'),
+
+  body('birthDate')
+    .optional()
+    .isDate().withMessage('Некорректная дата рождения'),
+
+  body('phone')
+    .optional()
+    .isLength({ max: 50 }).withMessage('Телефон не должен быть длиннее 50 символов')
+    .notEmpty().withMessage('Телефон не может быть пустым'),
+
+  body('email')
+    .optional()
+    .isEmail().withMessage('Некорректный email')
+    .isLength({ min: 5, max: 80 }).withMessage('Email должен быть от 5 до 80 символов'),
+
+  body('programmingLanguage')
+    .optional()
+    .isLength({ max: 100 }).withMessage('Язык программирования не должен быть длиннее 100 символов')
+    .notEmpty().withMessage('Язык программирования не может быть пустым'),
+
+  body('country')
+    .optional()
+    .notEmpty().withMessage('Страна не может быть пустой'),
+
+  body('bankCard')
+    .optional()
+    .notEmpty().withMessage('Банковская карта не может быть пустой'),
+
   body('workingHoursPerWeek')
-  .optional()
-  .custom((value, { req }) => {
-    if (req.user.role !== 'admin') {
-      throw new Error('Поле "workingHoursPerWeek" можно изменять только администратору');
-    }
-    return true;
-  })
-  .isInt({ min: 0 })
-  .withMessage('Параметр "workingHoursPerWeek" должен быть положительным целым числом'),
+    .optional()
+    .custom((value, { req }) => {
+      if (req.user.role !== 'admin') {
+        throw new Error('Поле "workingHoursPerWeek" можно изменять только администратору');
+      }
+      return true;
+    })
+    .isInt({ min: 0, max: 100 })
+    .withMessage('Параметр "workingHoursPerWeek" должен быть положительным целым числом'),
 ];
 
 exports.userCreateValidation = [
-  body('email').isEmail().withMessage('Некорректный email'),
-  body('password').isLength({ min: 6 }).withMessage('Пароль должен содержать не менее 6 символов'),
-  body('firstName').notEmpty().withMessage('Имя обязательно'),
-  body('lastName').notEmpty().withMessage('Фамилия обязательна'),
+  body('email')
+    .isEmail().withMessage('Некорректный email')
+    .isLength({ min: 5, max: 80 }).withMessage('Email должен быть от 5 до 80 символов'),
+
+  body('password')
+    .isLength({ min: 6, max: 20 }).withMessage('Пароль должен быть от 6 до 20 символов'),
+
+  body('firstName')
+    .isLength({ min: 2, max: 40 }).withMessage('Имя должно быть от 2 до 40 символов')
+    .notEmpty().withMessage('Имя обязательно'),
+
+  body('lastName')
+    .isLength({ min: 2, max: 40 }).withMessage('Фамилия должна быть от 2 до 40 символов')
+    .notEmpty().withMessage('Фамилия обязательна'),
+
+  body('middleName')
+    .isLength({ min: 2, max: 40 }).withMessage('Отчество должно быть от 2 до 40 символов')
+    .optional(),
+
+  body('phone')
+    .optional()
+    .isLength({ max: 50 }).withMessage('Телефон не должен быть длиннее 50 символов'),
+
+  body('programmingLanguage')
+    .optional()
+    .isLength({ max: 100 }).withMessage('Язык программирования не должен быть длиннее 100 символов'),
+
   body('workingHoursPerWeek')
-  .optional()
-  .isInt({ min: 0 })
-  .withMessage('Параметр "workingHoursPerWeek" должен быть положительным целым числом'),
-  // Добавьте другие проверки по необходимости
+    .optional()
+    .isInt({ min: 0, max: 100 })
+    .withMessage('Параметр "workingHoursPerWeek" должен быть положительным целым числом'),
 ];
 
 
