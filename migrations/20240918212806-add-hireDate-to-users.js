107e15f09941af585de8c75d3e8c3be2a9f8d854
Alex <130073078+dreamquality@users.noreply.github.com>
2024-09-23 16:24:59 +0300

    Update 20240918212806-add-hireDate-to-users.js

--- migrations/20240918212806-add-hireDate-to-users.js
+++ migrations/20240918212806-add-hireDate-to-users.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Users', 'hireDate', {
-      type: Sequelize.DATE,
-      allowNull: false,  // Укажите, что поле не может быть null
-      defaultValue: Sequelize.NOW,  // По умолчанию текущая дата
-    });
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return queryInterface.addColumn('Users', 'hireDate', {
+//       type: Sequelize.DATE,
+//       allowNull: false,  // Укажите, что поле не может быть null
+//       defaultValue: Sequelize.NOW,  // По умолчанию текущая дата
+//     });
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Users', 'hireDate');
-  }
-};
+//   down: async (queryInterface, Sequelize) => {
+//     return queryInterface.removeColumn('Users', 'hireDate');
+//   }
+// };
