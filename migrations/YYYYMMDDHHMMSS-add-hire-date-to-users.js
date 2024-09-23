8642f7d497cb14eb5e50726b873a457529c4db5b
Alex <130073078+dreamquality@users.noreply.github.com>
2024-09-23 16:25:22 +0300

    Update YYYYMMDDHHMMSS-add-hire-date-to-users.js

--- migrations/YYYYMMDDHHMMSS-add-hire-date-to-users.js
+++ migrations/YYYYMMDDHHMMSS-add-hire-date-to-users.js
@@ -1,30 +1,30 @@
-// migrations/YYYYMMDDHHMMSS-add-hire-date-to-users.js
+// // migrations/YYYYMMDDHHMMSS-add-hire-date-to-users.js
 
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      {
-        tableName: 'Users',
-        schema: 'user_schema',
-      },
-      'hireDate',
-      {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('NOW()'),
-      }
-    );
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return queryInterface.addColumn(
+//       {
+//         tableName: 'Users',
+//         schema: 'user_schema',
+//       },
+//       'hireDate',
+//       {
+//         type: Sequelize.DATE,
+//         allowNull: false,
+//         defaultValue: Sequelize.literal('NOW()'),
+//       }
+//     );
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn(
-      {
-        tableName: 'Users',
-        schema: 'user_schema',
-      },
-      'hireDate'
-    );
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     return queryInterface.removeColumn(
+//       {
+//         tableName: 'Users',
+//         schema: 'user_schema',
+//       },
+//       'hireDate'
+//     );
+//   },
+// };
